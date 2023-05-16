6adb0c6344cee3958d09b9e843ef35b2cc13e835
Oluwabunmi Fife <oluwabunmifife@gmail.com>
2023-05-16 15:59:05 +0100

    Running raw sql queries

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //Inserting
+    // $user = DB::insert('insert into users (name, email, password) values (?, ?, ?)', 
+    //         ['Dayle', 'dayle@you.com', 'Passerword']);
+
+    //Selecting
+    $users = DB::select('select * from users ');
+
+    //Deleting
+    //$user = DB::delete('delete from users where id=3');
+
+    dd($users);
+    //return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
