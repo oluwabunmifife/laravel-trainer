b752bdf48a28631072892180289a183779182eab
Oluwabunmi Fife <oluwabunmifife@gmail.com>
2023-05-18 11:21:49 +0100

    Working with Eloquent

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+
+use Dotenv\Parser\Value;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,4 +45,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //Encrypt password on registration
+    protected function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn($value) => bcrypt($value)
+        );
+    }
+
+    //Return user name as UPPERCASE
+    // protected function name(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn($value) => strtoupper($value)
+    //     );
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -16,21 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-
-    $users = DB::table('users')->get();
-
-    //Inserting
-    // $user = DB::insert('insert into users (name, email, password) values (?, ?, ?)', 
-    //         ['Dayle', 'dayle@you.com', 'Passerword']);
-
-    //Selecting
-    //$users = DB::select('select * from users ');
-
-    //Deleting
-    //$user = DB::delete('delete from users where id=3');
-
-    dd($users);
-    //return view('welcome');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
